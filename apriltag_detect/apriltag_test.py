120695d6be75a17f52a04a8df6238e76a1349c0e
Gideon <gideoncarleton@gmail.com>
2022-10-13 00:04:33 -0700

    networktables used in test, not publishing

--- apriltag_detect/apriltag_test.py
+++ apriltag_detect/apriltag_test.py
@@ -3,6 +3,12 @@ import cv2
 import numpy as np
 import apriltag as ap
 
+#for networktables
+from networktables import NetworkTables
+#to see messages from networktables, you must setup logging
+import logging
+
+
 #Creating cam VideoCapture object to read USB camera
 """cam = cv2.VideoCapture(0)
 
@@ -33,25 +39,49 @@ while (cam.isOpened()):
 
 """
 
+#setting up networktables
+logging.basicConfig(level=logging.DEBUG)
+
+
+NetworkTables.initialize(server='127.0.0.2')
+
+def valueChanged(table, key, value, isNew):
+    print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
+
+
+def connectionListener(connected, info):
+    print(info, "; Connected=%s" % connected)
+
+
+NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
+
+sd = NetworkTables.getTable("SmartDashboard")
+sd.addEntryListener(valueChanged)
+table = NetworkTables.getTable("ArrayVals")
+
+
+
 distance = 0
 rotation = 0
 #translation in x and y are from center. upper left: (-,-), upper right: (+,-)
 # lower right: (+,+), lower left: (-,+)
-translation_x = [2,0]
-translation_y = [3,1]
-translation_z = [3,2]
+translation_x = (0,3)
+translation_y = (1,3)
+translation_z = (2,3)
 
 tag_size = 0.146 #tag size in meters
 
 frame_size = 1280, 800
 
 options = ap.DetectorOptions(families="tag36h11", nthreads=1, quad_contours=False)
-detector = ap.Detector(options)\
+detector = ap.Detector(options)
 
 image_path = 'photos/36h11tag.png'
 
 #import camera params: 
-with np.load('camera_calibration/CameraParams.npz') as file:
+#with np.load('camera_calibration/CameraParams.npz') as file:
+#needed to run from terminal:
+with np.load('~/py_ws/vision_tests/CameraParams.npz') as file:
     mtx, dist, rvecs, tvecs = [file[i] for i in ('cameraMatrix','dist','rvecs','tvecs')]
 
 # new camera matrix fx, fy, cx, cy
@@ -64,7 +94,7 @@ def point_to_coordinate(frame=array, points=array):
     point_2_xy = x, y
 
     return point_2_xy
-translation_x
+
 
 cam = cv2.VideoCapture(0)
 
@@ -88,13 +118,19 @@ while(cam.isOpened):
         print("Apriltag detected")
 
         #detect pose of the tag
-        pose = detector.detection_pose(results[0], cam_params, tag_size)
+        detection_array = detector.detection_pose(results[0], cam_params, tag_size)
+        pose = detection_array[0]
         
         #getting information from the detection. 
         #possible outputs: center, corner, tag_family, tag_id, 
         #DetectionBase, hamming, goodness, decision_margin, homography
         tf = results[0].tag_family
         tid = results[0].tag_id
+
+        #send detection pose x, y, &z to networktables
+        table.putNumber("TransX", pose[translation_x])
+        table.putNumber("TransY", pose[translation_y])
+        table.putNumber("TransZ", pose[translation_z])
 
 
         """print(tf)
@@ -111,7 +147,8 @@ while(cam.isOpened):
 
         print("camera matrix:")
         print(mtx[0,0])"""
-        print(pose[0])
+        print(detection_array[0])
+        print(pose[translation_x])
 
 
 
